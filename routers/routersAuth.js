f387c7eb11652d78b63f8e1851219851c7cc1adf
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-04-25 19:53:55 +0200

    añadido el checkToken para el futuro dentro de sus rutas

--- routers/routersAuth.js
+++ routers/routersAuth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { checkSchema } = require('express-validator');
 const { validateInputs } = require('../middleware/inputValidator');
 const { createEditUserSchema } = require('../helpers/schemaUserValidator')
-
+const { checkToken }= require('../middleware/validarToken')
 
 module.exports = router;
